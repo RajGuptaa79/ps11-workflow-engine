33b1f9f75bcfd3e6f23b0580911922ce294537ae
Mayank Saini <mayank.saini.tech@gmail.com>
2026-08-24 03:37:40 +0530

    All frontend code

---
+++ Frontend/Landing page/landing-page-flowy/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import AuthPage from "./pages/AuthPage";
+import LogoutPage from "./pages/logOutPage";
+
+function App() {
+  return (
+    <>
+      <LogoutPage></LogoutPage>
+    </>
+  );
+}
+
+export default App;
